f1f11701c37b6f94ecb16753952f2d247f786279
Monika Mielnicka <monika.mielnicka.10@aberdeen.ac.uk>
2016-06-09 16:47:25 +0200

    gRNA search to a file added

--- crispr.py
+++ crispr.py
@@ -4,29 +4,48 @@
 #input sequence as [scaffold000] and define range [0:0]
 
 from Bio import SeqIO
-
 import re
 
+'''
+
 Seaurchin_dict = SeqIO.to_dict(SeqIO.parse('seaurchinscaffolds.fa','fasta'))
-sequences = (Seaurchin_dict['Scaffold670'] [107800:107840])
+sequences = (Seaurchin_dict['Scaffold670'] [107780:107850])
 print(sequences.seq)
 output_handle = open ('DNA_Scaffold', 'w') 
 SeqIO.write(sequences, output_handle,'fasta')
 output_handle.close()
-
+'''
 #PAM finder + 20nt
-new_sequences = 'TAGAGACGCGCCGAAAAGAACTGTACGGACTGTAGCCACC'
+#new_sequences = raw_input ('enter DNA for gRNA scan: ')
+new_sequences = 'ACTAAGTACTGAGCTACTCCTAGAGACGCGCCGAAAAGAACTGTACGGACTGTAGCCACCCTATGACGTC'
 
-for m in re.finditer('[ATGC]CC', new_sequences):
-	print('PAM found', m.start(), m.end())
-	print(new_sequences[m.start()-20:m.end()])
+OutFile = open('NGG_gRNA', 'w')
+
+for m in re.finditer('[ATGC]GG', new_sequences): 
+	OutFile.write('NGG PAM found '+ str(m.start())+ str(m.end()) + new_sequences[m.start()-20:m.end()])
+	print('NGG PAM found', m.start(), m.end()) 
+
+#if len(new_sequences)-m.start() > 20:
+	print(new_sequences[m.start()-20:m.end()]) 
+	
+#else:
+	#print('NGG no valid sequence match')  
+
+for m in re.finditer('CC[ATGC]', new_sequences): 
+	OutFile.write('CCN PAM found '+ str(m.start())+ str(m.end()) + new_sequences[m.start()-20:m.end()])
+	print('CCN PAM found', m.start(), m.end()) 
+#if len(new_sequences)-m.end() > 20:	
+	print(new_sequences[m.start():m.end()+20])
+
+#else: 	
+	#print('CCN no valid sequence match')
+
+OutFile.close()
+
+#PAM BLAST - this finds the blast sequence with maximum 15/20 mismatches 
 
 #pat = re.compile ('[ATGC]CC')
 #match = pat.search (new_sequences)
-
-         
-
-
 '''
 how to find all occurnces
 if len(new_sequences)-match.start() < 20:
